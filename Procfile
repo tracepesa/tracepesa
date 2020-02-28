80aeab4d0bf16fc333ec3f01d388b2efe21512f3
Samson Shirima@Home <shirimas@gmail.com>
2020-02-28 15:55:30 +0300

    Add Profile

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn tracepesa.wsgi
--- tracepesa/settings.py
+++ tracepesa/settings.py
@@ -115,7 +115,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media')
