db153e03eb3f431e1b58ccc61c1868e71fc03712
Samson Shirima@Home <shirimas@gmail.com>
2020-02-28 16:04:33 +0300

    Add host to the allowed hosts

--- tracepesa/settings.py
+++ tracepesa/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'sy4c#tewyj5h_3ag9_kuf=!99w))ujy$+5cwimwr65qf#n^xow'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['tracepesa.herokuapp.com']
 
 # Application definition
 
