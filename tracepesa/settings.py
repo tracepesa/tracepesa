71cc272f15e4aea2a0f416c1f25c4acbc0268085
Samson Shirima@Home <shirimas@gmail.com>
2020-02-28 22:53:29 +0300

    Modification of staticfiles

--- tracepesa/settings.py
+++ tracepesa/settings.py
@@ -128,8 +128,4 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, 'static'),
-]
-
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
