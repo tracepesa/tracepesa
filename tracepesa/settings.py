b63a1b874ed88377c9d8d8f5507393e4e6c4b7ed
Samson Shirima@Home <shirimas@gmail.com>
2020-02-28 16:17:52 +0300

    Add host to the allowed hosts

--- tracepesa/settings.py
+++ tracepesa/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -122,3 +123,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media')
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
+django_heroku.settings(locals())
